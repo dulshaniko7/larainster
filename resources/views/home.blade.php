768105ff6a73802f42b24f627c00c5eff9df566c
abc <abc@example.com>
2020-04-27 09:58:12 +0530

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <img src="/svg/airplane-outline.svg" alt="" style="max-width: 50px">
+            </div>
+            <div class="col-9">
+                <div><h1>freecodecamp</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>156</strong>posts</div>
+                    <div class="pr-5"><strong>32k</strong>followers</div>
+                    <div class="pr-5"><strong>32k</strong>following</div>
+                </div>
+                <hr/>
+                <div>FREECODECAMP</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, deleniti!</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-4">
+                <img src="/images/duncan-shaffer-IKXlvKIcdQU-unsplash.jpg" alt="" class="w-75">
+            </div>
+            <div class="col-4">
+                <img src="/images/joao-victor-fonseca-CLnwQUwGpBE-unsplash%20(1).jpg" alt="" class="w-75">
+            </div>
+            <div class="col-4">
+                <img src="/images/sayla-brown-QNQeYpZA15s-unsplash.jpg" alt="" class="w-75">
+            </div>
+        </div>
+    </div>
+@endsection
